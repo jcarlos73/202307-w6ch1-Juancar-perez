e090fb92cb246e08d1c7ba5374dab069bc60bbd4
Juan Carlos Pérez <jcparetas@gmail.com>
2023-08-15 18:25:18 +0200

    Create FilmsList.tsx

--- src/components/FilmsList/FilmsList.tsx
+++ src/components/FilmsList/FilmsList.tsx
@@ -1,21 +1,20 @@
+import { useContext } from "react";
+import FilmsContext from "../../store/films/context/filmsContext";
 import FilmCard from "../FilmCard/FilmCard";
 import "./FilmsList.css";
 
 const FilmsList = (): React.ReactElement => {
+  const { films } = useContext(FilmsContext);
+
   return (
     <ul className="films">
-      <li>
-        <FilmCard />
-      </li>
-      <li>
-        <FilmCard />
-      </li>
-      <li>
-        <FilmCard />
-      </li>
-      <li>
-        <FilmCard />
-      </li>
+      {films.map((film) => {
+        return (
+          <li key={film.id}>
+            <FilmCard film={film} />
+          </li>
+        );
+      })}
     </ul>
   );
 };
