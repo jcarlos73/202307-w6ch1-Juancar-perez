6c8f55256ce728411aa907057ed60881fac913e9
Juan Carlos Pérez <jcparetas@gmail.com>
2023-08-15 16:19:02 +0200

    Test FilmListPage and import testing library to setupTests.ts

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./components/App/App";
+import FilmsContextProvider from "./store/films/context/FilmsContextProvider";
 import "./styles/styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <FilmsContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </FilmsContextProvider>
   </React.StrictMode>,
 );
