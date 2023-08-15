e83c6a35e9bc3c1ff8a3c0d2e671649c94dcf060
Juan Carlos Pérez <jcparetas@gmail.com>
2023-08-15 10:53:29 +0200

    Modify useFilmsApi

--- src/hooks/useFilmsApi.ts
+++ src/hooks/useFilmsApi.ts
@@ -5,14 +5,10 @@ const useFilmsApi = () => {
   const apiUrl = `${import.meta.env.VITE_FILMS_API_URL}/films`;
 
   const getFilmsApi = useCallback(async () => {
-    try {
-      const response = await fetch(apiUrl);
-      const apiFilms = (await response.json()) as Film[];
+    const response = await fetch(apiUrl);
+    const apiFilms = (await response.json()) as Film[];
 
-      return apiFilms;
-    } catch (error) {
-      throw new Error("Can´t get films");
-    }
+    return apiFilms;
   }, [apiUrl]);
 
   return {
